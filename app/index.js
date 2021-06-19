3cbef96023a6f51ff34185038c293ad695c11652
Marcos Victor <marcos@conveniar.com.br>
2021-06-19 20:11:43 -0300

    primeiro commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+// const routes = require('./routes')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+   res.send('iniciado')
+})
+
+app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
