aafc2fd1d461742b28b454b2782b82063d04003f
Marcos Victor <marcos@conveniar.com.br>
2021-07-12 19:55:02 -0300

    removido trecho comentado

--- app/models/project.js
+++ app/models/project.js
@@ -4,19 +4,11 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Project extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Project.hasOne(models.Tasks, {
         foreignKey: 'id_project'
       })
-      // Project.belongsToMany(models.Users, {
-      //   through: 'Users_Projects',
-      //   foreignKey: 'id_projects'
-      //  })
     }
   };
   Project.init({
