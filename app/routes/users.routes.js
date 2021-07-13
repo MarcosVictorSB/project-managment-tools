cc938a99fde443d703ed4a77a8d7cd208429b79f
Marcos Victor <marcos@conveniar.com.br>
2021-07-13 14:59:05 -0300

    rota para pegar todos os usuarios

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,11 +1,13 @@
  const Router = require('express')
+const UsersController = require('../controllers/UserController')
  const UserController = require('../controllers/UserController')
  const auth = require('../middleware/auth')
 
  const routes = Router()
 
  routes
-   .get('/usuarios', auth, UserController.getAllUser)
+   .get('/usuarios', auth, UserController.getUserActive)
+   .get('/usuarios/todos', auth, UserController.getAllUser)
    .post('/usuarios/criar',auth,  UserController.createNewUser)
    .get('/usuarios/:id', auth, UserController.getOneUser)
    .delete('/usuarios/:id', auth, UserController.deleteUser)
