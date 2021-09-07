2611c3a218f1875724b82cda2a0a15ef2e0bd749
Marcos Victor <marcos@conveniar.com.br>
2021-09-07 20:22:59 -0300

    definido a porta

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 const path = require('path')
 
 const app = express()
-const port = 3000
+
 
 // app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname, './app/views'))
