327e884e0b934bec64f42217a973ed54c92e24fc
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-06-02 18:26:45 -0300

    feat: add the logger on aplication

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
 const path = require('path')
+const logger = require('./helpers/logger');
 
 const app = express()
+app.use(logger)
+
 
 routes(app)
 
