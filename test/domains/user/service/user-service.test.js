65eceb38df2e023d7f8b1073a3c47e73186ce2c5
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-06-03 09:03:29 -0300

    chore: unit test with sinon

--- test/domains/user/service/user-service.test.js
+++ test/domains/user/service/user-service.test.js
@@ -1,3 +1,5 @@
+const sinon = require('sinon');
+const { expect } = require('chai')
 const factory = require('../../../../src/domains/user/factories')
 
 describe('User Service', () => {
@@ -10,18 +12,19 @@ describe('User Service', () => {
     this.params = {
       email: 'any_email'
     }
-    this.repository = factory.getUserRepository();
-    this.repository = {
-      getUserBy: jest.fn().mockResolvedValue(this.user)
-    }
+    
+    this.service.repository = {
+      getUserBy: sinon.stub().resolves(this.user)
+    } 
   })
 
-  it('return status code 400 and already exist user', async () => {
-    const response = await this.service.create(this.params.email)    
+  it('return status code 409 and already exist user', async () => {
+    const response = await this.service.create(this.params.email)   
     
-    expect(response.status).toEqual(409)
-    expect(response.body.error).toEqual('already exists user with this email')
-    // expect(this.repository.getUserBy).toHaveBeenCalledWith(this.params.email)
+    expect(response.status).to.eql(409)
+    expect(response.body.error).to.eql('already exists user with this email')
+    sinon.assert.calledOnce(this.service.repository.getUserBy)
+    sinon.assert.calledOnceWithExactly(this.service.repository.getUserBy, this.params.email)
     
   })
 })
