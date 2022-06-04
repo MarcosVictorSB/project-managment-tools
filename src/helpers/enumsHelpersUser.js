5afc9575633e18e178ecabe5e1ed5a6d90cdddeb
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-06-04 18:49:03 -0300

    chore: add status code 500 and export enum HttpStatusCode and create enumsHelper

---
+++ src/helpers/enumsHelpersUser.js
@@ -0,0 +1,9 @@
+const enumsHelpesUser = {
+  user: {
+    alreadyExists: 'already exists user with this email',
+    errorToCreatedUser: 'error created user'
+  }
+}
+
+
+module.exports = enumsHelpesUser;
--- src/protocols/http.js
+++ src/protocols/http.js
@@ -1,24 +1,28 @@
 const HttpStatusCode = {
   Created: 201,
-  Conflict: 409
+  Conflict: 409,
+  serverError: 500
 }
 
 
 const conflict = (error) => ({
   status: HttpStatusCode.Conflict,
-  body: {
-    error: error
-  }
+  body: { error }
 })
 
 const created = (response) => ({
   status: HttpStatusCode.Created,
-  body: {
-    result: response,
-  }
+  body: { result: response }
+})
+
+const serverError = (error) => ({
+  staus: HttpStatusCode.serverError,
+  body:{ error }
 })
 
 module.exports = {
+  HttpStatusCode,
   created,
-  conflict
+  conflict,
+  serverError
 }
