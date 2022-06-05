1058bbf273ef1d16c9d63a39b6e4ad63906de9c3
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-06-05 02:10:51 -0300

    chore: delete enum helper

--- src/helpers/enumsHelpersUser.js
+++
@@ -1,9 +0,0 @@
-const enumsHelpesUser = {
-  user: {
-    alreadyExists: 'already exists user with this email',
-    errorToCreatedUser: 'error created user'
-  }
-}
-
-
-module.exports = enumsHelpesUser;
