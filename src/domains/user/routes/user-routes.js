33abcf528d64a846f97d902a430ddea5ef31c324
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-06-11 17:59:53 -0300

    chore: indent user route

--- src/domains/user/routes/user-routes.js
+++ src/domains/user/routes/user-routes.js
@@ -3,11 +3,11 @@ const factory = require('../factories')
 const controller = factory.getUserController();
 
 const routes = Router()
-  routes
-    .post('/users/', (...args) => controller.create(...args))
-    .get('/users/:id', (...args) => controller.getById(...args))
-    .get('/users/', (...args) => controller.getAllUser(...args))
-    .put('/users/:id', (...args) => controller.update(...args))
-    .delete('/users/:id', (...args) => controller.delete(...args))
+routes
+  .post('/users/', (...args) => controller.create(...args))
+  .get('/users/:id', (...args) => controller.getById(...args))
+  .get('/users/', (...args) => controller.getAllUser(...args))
+  .put('/users/:id', (...args) => controller.update(...args))
+  .delete('/users/:id', (...args) => controller.delete(...args))
 
 module.exports = routes
