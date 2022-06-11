f22609d3b2a7a93b351b5d07963265d113abd9cc
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-06-10 22:33:52 -0300

    feat: create enum update user

--- src/helpers/enumHelperUser.js
+++ src/helpers/enumHelperUser.js
@@ -3,6 +3,7 @@ const enumHelperUser = {
     alreadyExists: 'already exists user with this email',
     errorToCreatedUser: 'error created user',
     notFoundUser: 'user not found',
+    updateUser: 'user update'
   }
 }
 
